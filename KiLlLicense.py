31ce2ac5cd8b60c46aaa02b405f9dd9f0ff2eea7
NyafkA :3 <lolifox@riseup.net>
2020-12-12 21:14:20 +0300

    Update KiLlLicense.py

--- KiLlLicense.py
+++ KiLlLicense.py
@@ -14,7 +14,6 @@ import docx
 import time
 
 
-
 #============= PROGRESS BAR ================================
 # SOURCE: https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -39,36 +38,25 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
         print()
         
 #=================================================  
-
 #=============================ДАТА ДЕЙСТВИЯ ЛИЦЕНЗИИ
 now = datetime.now()
-
 two = timedelta(days=730)
-
-
 dead = (now - two)
 pred = timedelta(days=62)
 dead2 = (now + pred)
 #=====================================================ГЛАВНЫЙ ЭКРАН И ФУНКЦИЯ СТАРТ
-
-
-                                                                                             
+                                                                                     
 print("Приветствую, сегодня: " + now.strftime("%d.%m.%y"))              
 print("Крайний срок равен: " + str(dead))
 
 def age_sort():
-
-    
     a = int(input("Введите с: "))
     b = int(input("Введите до: "))
     #=======================================ЧТЕНИЕ ФАЙЛА И ЗАПУСК ЦИКЛА ДЛЯ ПРОХОДА ПО РЯДКАХ ТАБЛИЦЫ
 ######
     doc = docx.Document('database.docx')  #Открытие файла
-   
     table = doc.tables[0]      #Инициализация таблицы
-    
     count_rows = 0 #Количество рядков
-
     doc_output2 = docx.Document() #Создание нового файла
     doc_output2.add_paragraph('Этот файл был создан автоматически!\nВремя создания: ' + str(now)) #а почему бы и нет
     doc_output2.add_paragraph(f"Результаты сортировки по возрасту от {a} до {b} лет") 
@@ -84,8 +72,6 @@ def age_sort():
     table_output3.rows[0].cells[5].text = "Разряд"
     table_output3.rows[0].cells[6].text = "Лицензия"
     table_output3.rows[0].cells[7].text = "Приказ"
-    
-    
     #doc_output.save("Файл KILLIST_" + str(now.strftime("%d.%m.%y")) + ".docx")
 #=======================================
 
@@ -95,27 +81,18 @@ def age_sort():
     # Инициализация прогресс бара
     printProgressBar(0, count_rows, prefix = 'Прогресс:', suffix = 'Выполнено', length = 50)    
 
-
     i = 0  # Счетчик для прогрес бара
     time_start=time.time()  # Получаем начальное время выполнения операции 
 
     
 #===========Цикл прохода по строчкам таблицы + отрисовка прогресс бара    
     for row in table.rows[1:]:
-        
-        
         birth_date = row.cells[2].text
-        
-        
-        
         #print(f'Данные из таблицы: ФИО - {name}; Дата рождения - {birth_date}; Тренер - {trainer}; Дата зачисления - {in_date}; Группа - {group}; Разряд - {category}; Лицензия - {license}; Приказ - {order}. ') 
-        
         # ОБНОВЛЕНИЕ ПРОГРЕСС БАРА
         i += 1 # Счетчик для прогрес бара
         printProgressBar(i + 1, count_rows+1, prefix = 'Прогресс:', suffix = 'Выполнено', length = 50)
 
-
-    
 #====================Проверка и конвертирование даты лицензии
         if birth_date == '':
             continue
@@ -128,7 +105,6 @@ def age_sort():
         #над данной строчкой нужно подумать
         v = days // 365
         if v > a and v < b:
-            
         
             name = row.cells[1].text
             
@@ -149,9 +125,7 @@ def age_sort():
             table_output3.rows[-1].cells[7].text = order
             #print(f'Пользователь: {name} подходит под условия сортировки')
             
-             
     print(f' Время выполнения операции: {int(time.time()-time_start)} секунд') #Получаем конечное время
-    
     
 #============Вывод сообщения и документа со списком тех, кто просрочил лицензицю
     
@@ -160,8 +134,6 @@ def age_sort():
     back = input('\nНажмите "1" что-бы вернуться к меню: ')
     if back == "1":
       clear() 
-      
-
 
 def start():
   while True:
@@ -203,16 +175,13 @@ def start():
       clear()
       age_sort()
       pass
-    
 
 #=======================================ЧТЕНИЕ ФАЙЛА И ЗАПУСК ЦИКЛА ДЛЯ ПРОХОДА ПО РЯДКАХ ТАБЛИЦЫ
 ######
 
 def licence_dead():
     doc = docx.Document('database.docx')  #Открытие файла
-   
     table = doc.tables[0]      #Инициализация таблицы
-    
     count_rows = 0 #Количество рядков
 #=======================================СОЗДАНИЯ ФАЙЛА И ТАБЛИЦЫ    ДЛЯ ЗАПИСИ В ТАБЛИЦУ
 
@@ -232,8 +201,6 @@ def licence_dead():
     table_output.rows[0].cells[6].text = "Лицензия"
     table_output.rows[0].cells[7].text = "Приказ"
     
-    
-    
     #################################################Таблица с предупреждениям
     doc_output.add_paragraph("\n Далее показаны пользователи у которых лицензия закончится в течении следующих двух месяцев!") 
     doc_output.add_table(1,8) #ну колонок то у нас 8
@@ -248,7 +215,6 @@ def licence_dead():
     table_output2.rows[0].cells[6].text = "Лицензия"
     table_output2.rows[0].cells[7].text = "Приказ"
     
-    
     #doc_output.save("Файл KILLIST_" + str(now.strftime("%d.%m.%y")) + ".docx")
 #=======================================
 
@@ -258,34 +224,23 @@ def licence_dead():
     # Инициализация прогресс бара
     printProgressBar(0, count_rows, prefix = 'Прогресс:', suffix = 'Выполнено', length = 50)    
 
-
     i = 0  # Счетчик для прогрес бара
     time_start=time.time()  # Получаем начальное время выполнения операции 
 
-    
 #===========Цикл прохода по строчкам таблицы + отрисовка прогресс бара    
     for row in table.rows[1:]:
-        
-        
-        
         license = row.cells[7].text 
-        
-        
         #print(f'Данные из таблицы: ФИО - {name}; Дата рождения - {birth_date}; Тренер - {trainer}; Дата зачисления - {in_date}; Группа - {group}; Разряд - {category}; Лицензия - {license}; Приказ - {order}. ') 
-        
         # ОБНОВЛЕНИЕ ПРОГРЕСС БАРА
         i += 1 # Счетчик для прогрес бара
         printProgressBar(i + 1, count_rows+1, prefix = 'Прогресс:', suffix = 'Выполнено', length = 50)
 
-
-    
 #====================Проверка и конвертирование даты лицензии
         if license == '':
             continue
         if len(license) >8:
             license = license[0:8]
         date_object = datetime.strptime(license, "%d.%m.%y")
-        
 
         if date_object < dead:
             name = row.cells[1].text
@@ -325,21 +280,12 @@ def licence_dead():
                 table_output2.rows[-1].cells[6].text = license
                 table_output2.rows[-1].cells[7].text = order
                 print(f'Для пользователя: {name} действие лицензии закончится в течении 2 месяцев! Дата покупки {license}')
-             
-
-
-
-
-             
     print(f' Время выполнения операции: {int(time.time()-time_start)} секунд') #Получаем конечное время
-    
     
 #============Вывод сообщения и документа со списком тех, кто просрочил лицензицю
     
     doc_output.save("Файл KILLIST_" + str(now.strftime("%d.%m.%y")) + ".docx")
     print("Файл KILLIST_" + str(now.strftime("%d.%m.%y")) + ".docx сохранен")
-    
-    
     
 #========================================Возвращение к главному меню
     back = input('\nНажмите "1" что-бы вернуться к меню: ')
